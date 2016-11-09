0517d6c3c80e53dd7ae93ea5539c5e3123e59e6f
Senthilselva <senthilselvak@gmail.com>
2016-11-08 21:21:44 -0500

    trying Heroku

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,20 @@
 Here is where you make the connection to the database and export and used by the O.R.M.
 */
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+var connection;
+
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+connection= mysql.createConnection({
 	port: 3306,
 	host: 'localhost',
 	user: 'root',
 	password: 'root',
 	database: 'burgers_db'
 });
+}
+
 
 connection.connect(function (err) {
 	if (err) {
